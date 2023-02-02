c6abedba94a4e3ce666d8d98a93e69c220d15082
Isabela Alves <108693206+Isabela-Alves@users.noreply.github.com>
2023-02-02 18:46:12 -0300

    criando o formulario de cadastro

---
+++ formulario.php
@@ -0,0 +1,153 @@
+<?php
+
+     /*esta verificando se existe essa variavel,ai quando clicar em enviar vai exitir essa*/
+    {
+        
+        
+    
+        require_once('config.php');
+        
+        $nome = $senha = $email =$estado = $msg = "";
+if (isset($_POST['submit'])) {
+    if (isset($_POST['nome'])) {
+        $nome =  $_POST['nome'];
+    }
+    if (isset($_POST['senha'])) {
+        $senha =  $_POST['senha'];
+    }
+    if (isset($_POST['email'])) {
+        $email =  $_POST['email'];
+    }
+    if (isset($_POST['estado'])) {
+        $estado =  $_POST['estado'];
+    }
+}
+     
+        $query = "INSERT INTO  autentificacao (nome, senha, email,estado ) VALUES ('$nome', '$senha','$email','$estado' )";
+        $result = mysqli_query($conexao, $query);
+        if ($result) {
+            $msg = '<div class="alert alert-success" role="alert">
+            Weldone! Reocrd added  successfully 
+          </div>';
+        } else {
+            $msg = '<div class="alert alert-warning" role="alert">
+            Error occurs while updating the query
+          </div>';
+    
+
+    
+
+
+        
+        
+    }
+}
+   
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário | GN</title>
+    <style>
+        body{
+            font-family: Arial, Helvetica, sans-serif;
+            background-image: linear-gradient(to right, rgb(20, 147, 220), rgb(17, 54, 71));
+        }
+        .box{
+            color: white;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%,-50%);
+            background-color: rgba(0, 0, 0, 0.6);
+            padding: 15px;
+            border-radius: 15px;
+            width: 20%;
+        }
+        fieldset{
+            border: 3px solid dodgerblue;
+        }
+        legend{
+            border: 1px solid dodgerblue;
+            padding: 10px;
+            text-align: center;
+            background-color: dodgerblue;
+            border-radius: 8px;
+        }
+        .inputBox{
+            position: relative;
+        }
+        .inputUser{
+            background: none;
+            border: none;
+            border-bottom: 1px solid white;
+            outline: none;
+            color: white;
+            font-size: 15px;
+            width: 100%;
+            letter-spacing: 2px;
+        }
+        .labelInput{
+            position: absolute;
+            top: 0px;
+            left: 0px;
+            pointer-events: none;
+            transition: .5s;
+        }
+        .inputUser:focus ~ .labelInput,
+        .inputUser:valid ~ .labelInput{
+            top: -20px;
+            font-size: 12px;
+            color: dodgerblue;
+        }
+        #submit{
+            background-image: linear-gradient(to right,rgb(0, 92, 197), rgb(90, 20, 220));
+            width: 100%;
+            border: none;
+            padding: 15px;
+            color: white;
+            font-size: 15px;
+            cursor: pointer;
+            border-radius: 10px;
+        }
+        #submit:hover{
+            background-image: linear-gradient(to right,rgb(0, 80, 172), rgb(80, 19, 195));
+        }
+    </style>
+</head>
+<body>
+    <a href="home.php">Voltar</a>
+    <div class="box">
+        <form action="formulario.php" method="POST">
+            <fieldset>
+                <legend><b>Fórmulário</b></legend>
+                <br>
+                <div class="inputBox">
+                    <input type="text" name="nome" id="nome" class="inputUser" required>
+                    <label for="nome" class="labelInput">Nome completo</label>
+                </div>
+                <br>
+                <div class="inputBox">
+                    <input type="password" name="senha" id="senha" class="inputUser" required>
+                    <label for="senha" class="labelInput">Senha</label>
+                </div>
+                <br><br>
+                <div class="inputBox">
+                    <input type="text" name="email" id="email" class="inputUser" required>
+                    <label for="email" class="labelInput">Email</label>
+                </div>
+                <br><br>
+                <div class="inputBox">
+                    <input type="text" name="estado" id="estado" class="inputUser" required>
+                    <label for="estado" class="labelInput">Estado</label>
+                </div>
+                <br><br>
+                <input type="submit" name="submit" id="submit">
+            </fieldset>
+        </form>
+    </div>
+</body>
+</html>
